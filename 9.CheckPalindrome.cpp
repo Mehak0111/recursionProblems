026d82d0b8b2d4827483695a3b2ec8254a303152
Mehak <77800400+Mehak0111@users.noreply.github.com>
2022-12-04 13:58:00 +0530

    Add files via upload

---
+++ 9.CheckPalindrome.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+bool checkPalindrome(string str,int i,int j){
+	if(i>j){
+		return true;
+	}
+	if(str[i]!=str[j])
+	return false;
+	else{
+		return checkPalindrome(str,i+1,j-1);
+	}
+}
+int main()
+{
+	string s;
+	getline(cin,s);
+	bool isPalindrome=checkPalindrome(s,0,s.length()-1);
+	if(isPalindrome){
+		cout<<"It is Palindrome"<<endl;
+	}
+	else{
+		cout<<"It is not a palindrome";
+	}
+}
