5e38715385fac6621e7884fff91f4a67b35df364
Mehak <77800400+Mehak0111@users.noreply.github.com>
2022-12-04 13:57:43 +0530

    Add files via upload

---
+++ 8.reverseString.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void reverse(string& s,int i,int j)
+{
+   if(i>j){
+   	return;
+   }
+   swap(s[i],s[j]);
+   i++;
+   j--;
+   reverse(s,i,j);
+}
+int main()
+{
+	string s;
+	getline(cin,s);
+	int name=reverse(s,0,s.length()-1);
+	cout<<name;
+}
